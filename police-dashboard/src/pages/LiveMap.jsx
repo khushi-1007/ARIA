821dfb7ae36a7ba477c21b595c1f146b7e970823
Durgesh Sharma <cambdrige.sharma.com@gmail.com>
2026-09-09 13:01:15 +0530

    Change tile layer source for LiveMap component

--- police-dashboard/src/pages/LiveMap.jsx
+++ police-dashboard/src/pages/LiveMap.jsx
@@ -45,11 +45,16 @@ export default function LiveMap() {
     if (!mapInstance.current && mapRef.current) {
       mapInstance.current = L.map(mapRef.current).setView([28.6139, 77.2090], 12);
 
-      L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 20
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 19
       }).addTo(mapInstance.current);
+
+      setTimeout(() => {
+        if (mapInstance.current) {
+          mapInstance.current.invalidateSize();
+        }
+      }, 150);
     }
 
     return () => {
